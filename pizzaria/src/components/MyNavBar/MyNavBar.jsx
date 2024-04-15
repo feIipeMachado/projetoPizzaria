4eaa1e0c1cb07ff3d03c999bfa7936a54cf9801b
felipe machado <machadofelipe677@gmail.com>
2024-04-15 12:39:58 -0300

    Ajeitando a barra de navegação para ficar responsiva em diferentes dimensões

--- pizzaria/src/components/MyNavBar/MyNavBar.jsx
+++ pizzaria/src/components/MyNavBar/MyNavBar.jsx
@@ -7,13 +7,20 @@ import loginIcon from '../../assets/login-icon.svg'
 
 export function MyNavbar() {
 
-    
+
     return (
         <nav className='nav'>
+            <input type='checkbox' id='check'></input>
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+            </label>
             <a href='/' className='site-title'>
                 Pizzaria
             </a>
             <ul>
+                <li>
+                    <a href='/' class='active'>Home</a>
+                </li>
                 <li>
                     <a href='/cardapio' className='nav-link'>Cardápio</a>
                 </li>
@@ -24,10 +31,13 @@ export function MyNavbar() {
                     <a href='/sobre-nos' className='nav-link'>Sobre nós</a>
                 </li>
                 <li>
-                    <Button href='/login' className='buttonLogin' variant="outlined" 
-                    startIcon={<img src={loginIcon} alt="Login" />}>
+                    <Button href='/login' className='buttonLogin' variant="outlined"
+                        startIcon={<img src={loginIcon} alt="Login" />}>
                         <span>Login</span>
                     </Button>
+                </li>
+                <li>
+                    <a href='/login' className='nav-link' class='inactive'>Login</a>
                 </li>
             </ul>
 
