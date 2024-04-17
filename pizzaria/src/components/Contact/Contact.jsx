36868f43f020483a56131c5d24d2ba2a462a3930
felipe machado <machadofelipe677@gmail.com>
2024-04-17 15:44:17 -0300

    Implementando carrossel na página home, editando a página de contato

--- pizzaria/src/components/Contact/Contact.jsx
+++ pizzaria/src/components/Contact/Contact.jsx
@@ -13,11 +13,53 @@ export function Contact({ isLoggedIn, handleLogout }) {
         <div>
             <MyNavbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
             <div>
-                <h1 className='whatsapptext'>Faça seu pedido via WhatsApp</h1></div>
+                <h1 className='whatsapptext'>Faça seu pedido via WhatsApp</h1>
+            </div>
+
             <a
                 aria-label="Chat on WhatsApp" href="https://wa.me/555193348443">
                 <img alt="Chat on WhatsApp" src={logoWhatsapp} className='logowhatsapp' />
             </a>
+
+            <div><h1 className='prices'>Nossos preços:</h1></div>
+
+            <div className='container'>
+                <ul className='list'>
+                    <li>
+                        <h2>Pizzas</h2>
+                    </li>
+                    <li>
+                        Média (35cm - 8 fatias) R$49,90
+                    </li>
+                    <li>
+                        Grande (40cm - 12 fatias) R$64,90
+                    </li>
+                    <li>
+                        Família (45cm - 16 fatias) R$79,90
+                    </li>
+                </ul>
+
+                <ul className='list'>
+                    <li>
+                        <h2>Bebidas</h2>
+                    </li>
+                    <li>
+                        Refrigerante 2 litros  R$10,00
+                    </li>
+                    <li>
+                        Água mineral 500ml R$3,00
+                    </li>
+                    <li>
+                        Água mineral 1 litro R$6,00
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h1 className='cardstext'>Horário de funcionamento:</h1>
+                <h2> Das 18:30 as 23:30</h2>
+            </div>
+
             <div>
                 <h1 className='cardstext'>Aceitamos:</h1></div>
             <a
@@ -44,6 +86,8 @@ export function Contact({ isLoggedIn, handleLogout }) {
                 aria-label="amexSvg">
                 <img alt="Cartões aceitos" src={logoHipercard} className='logoAmex' />
             </a>
+
         </div>
+        
     )
 }
--- pizzaria/src/components/Home/Home.jsx
+++ pizzaria/src/components/Home/Home.jsx
@@ -1,12 +1,45 @@
+import { useState } from 'react';
 import { MyNavbar } from '../MyNavBar/MyNavBar';
-
+import Carousel from 'react-bootstrap/Carousel';
+import slide1 from '../../assets/pizza-forna-lenha.png';
+import slide2 from '../../assets/pizza2.png'
+import '../Home/Home.css'
 
 export function Home({ isLoggedIn, isAdmin, handleLogout }) {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
     return (
         <div>
-            <MyNavbar isLoggedIn={isLoggedIn} handleLogout={handleLogout}/>
-            {isLoggedIn && <div>Olá, usuário</div>}
-            {isLoggedIn && isAdmin && <div>Olá, admin</div>}
-        </div>
+            <MyNavbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+            <Carousel activeIndex={index} onSelect={handleSelect} className="custom-carousel">
+                <Carousel.Item className="custom-carousel-item">
+                    <img
+                        src={slide1}
+                        alt="First slide"
+                        className="custom-carousel-image"
+                    />
+                    <Carousel.Caption className="custom-carousel-caption">
+                        <h3><a href="/contato">Pizzas artesanais de qualidade</a></h3>
+                        <p>Clique para fazer seu pedido agora.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="custom-carousel-item">
+                    <img
+                        className="custom-carousel-image"
+                        src={slide2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption className="custom-carousel-caption">
+                    <h3><a href="/cardapio">Grande variedade de sabores</a></h3>
+                        <p>Clique para ver o cardápio.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div >
     )
 }
+
